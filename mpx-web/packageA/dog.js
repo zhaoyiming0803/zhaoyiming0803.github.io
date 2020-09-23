ccbf3a0ab35aab9f48d58e4e5e205fed1ffe5a7e
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2020-09-23 11:15:30 +0800

    test

---
+++ mpx-web/packageA/dog.js
@@ -0,0 +1,70 @@
+
+var window = window || {};
+
+window["webpackJsonp"] = require("../bundle.js");
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[5],{
+
+/***/ 241:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(global) {/* mpx inject */ global.currentModuleId = "m4ec5ec96"
+global.currentResource = "/Users/didi/Desktop/github/test-mpx-web/src/packageA/dog.mpx"
+global.currentCtor = Component
+global.currentCtorType = "component"
+global.currentSrcMode = "wx"
+/* mpx inject */ global.currentInject = {
+  moduleId: "m4ec5ec96",
+  render: function () {
+    this._r();
+  }
+};
+/* harmony import */ var _babel_loader_node_modules_mpxjs_webpack_plugin_lib_selector_type_script_index_0_dog_mpx__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(242);
+/* empty/unused harmony star reexport */global.currentModuleId
+/* script */
+
+
+/* styles */
+/* json */
+__webpack_require__(243)
+
+/* template */
+__webpack_require__(244)
+
+
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(1)))
+
+/***/ }),
+
+/***/ 242:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _mpxjs_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(3);
+
+
+
+
+Object(_mpxjs_core__WEBPACK_IMPORTED_MODULE_0__["createPage"])({
+  onLoad: function onLoad() {}
+});
+
+/***/ }),
+
+/***/ 243:
+/***/ (function(module, exports) {
+
+// removed by extractor
+
+/***/ }),
+
+/***/ 244:
+/***/ (function(module, exports) {
+
+// removed by extractor
+
+/***/ })
+
+},[[241,1]]]);
---
+++ mpx-web/packageB/dog.js
@@ -0,0 +1,88 @@
+
+var window = window || {};
+
+window["webpackJsonp"] = require("../bundle.js");
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[6],{
+
+/***/ 245:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(global) {/* mpx inject */ global.currentModuleId = "m39eb48d2"
+global.currentResource = "/Users/didi/Desktop/github/test-mpx-web/src/packageB/dog.mpx"
+global.currentCtor = Component
+global.currentCtorType = "component"
+global.currentSrcMode = "wx"
+/* mpx inject */ global.currentInject = {
+  moduleId: "m39eb48d2",
+  render: function () {
+    this._c("dogAvatar", this.dogAvatar);
+    this._r();
+  }
+};
+/* harmony import */ var _babel_loader_node_modules_mpxjs_webpack_plugin_lib_selector_type_script_index_0_dog_mpx__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(246);
+/* empty/unused harmony star reexport */global.currentModuleId
+/* script */
+
+
+/* styles */
+/* json */
+__webpack_require__(248)
+
+/* template */
+__webpack_require__(249)
+
+
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(1)))
+
+/***/ }),
+
+/***/ 246:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _mpxjs_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(3);
+/* harmony import */ var static_images_dog_jpg_packageName_main__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(247);
+/* harmony import */ var static_images_dog_jpg_packageName_main__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(static_images_dog_jpg_packageName_main__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+
+
+Object(_mpxjs_core__WEBPACK_IMPORTED_MODULE_0__["createPage"])({
+  data: {
+    dogAvatar: static_images_dog_jpg_packageName_main__WEBPACK_IMPORTED_MODULE_1___default.a,
+    pageName: 'packageB/dog'
+  },
+  onLoad: function onLoad() {},
+  onPageScroll: function onPageScroll(e) {
+    console.log('onPageScroll: ', e);
+  }
+});
+
+/***/ }),
+
+/***/ 247:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/dog6711ed1a87a7423d586868f996ad4587.jpg";
+
+/***/ }),
+
+/***/ 248:
+/***/ (function(module, exports) {
+
+// removed by extractor
+
+/***/ }),
+
+/***/ 249:
+/***/ (function(module, exports) {
+
+// removed by extractor
+
+/***/ })
+
+},[[245,1]]]);
