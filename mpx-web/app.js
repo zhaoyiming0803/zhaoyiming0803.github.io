ff1ee3d6a48023b33ebd1ada6c45acd1d133b5c8
eamonzhaoyiming <eamonzhaoyiming@didiglobal.com>
2020-10-17 23:04:55 +0800

    test

--- mpx-web/app.js
+++ mpx-web/app.js
@@ -24324,7 +24324,7 @@ var MpxScroll = function () {
 
       var position = Object(_dom__WEBPACK_IMPORTED_MODULE_6__[/* getScrollTop */ "c"])();
 
-      return;
+      return window.scrollTo(0, _scrollTop);
 
       this.scrollAnimation.easeOutQuart(duration, position, _scrollTop, function (distance) {
         window.scrollTo(0, distance);
@@ -30545,7 +30545,7 @@ exports = module.exports = __webpack_require__(14)(false);
 
 
 // module
-exports.push([module.i, "\n.test-container {\n  margin-bottom: 20px;\n  width: 100%;\n  height: 1000px;\n  background-color: #f00;\n}\n.tabbar {\n  position: fixed;\n  top: 20px;\n  left: 0;\n  width: 50px;\n  height: 50px;\n  line-height: 50px;\n  text-align: center;\n  background: #00f;\n  color: #fff;\n}\n.box {\n  top: 0;\n  left: 0;\n  width: 100%;\n}\n", ""]);
+exports.push([module.i, "\n.test-container {\n  margin-bottom: 20px;\n  width: 100%;\n  height: 1000px;\n  background-color: #00f;\n}\n.tabbar {\n  position: fixed;\n  top: 20px;\n  left: 0;\n  width: 50px;\n  height: 50px;\n  line-height: 50px;\n  text-align: center;\n  background: #00f;\n  color: #fff;\n}\n.box {\n  top: 0;\n  left: 0;\n  width: 100%;\n}\n", ""]);
 
 // exports
 
