bc6ee40584720ef97db84bd7f06255ccf35f63b2
eamonzhaoyiming <eamonzhaoyiming@didiglobal.com>
2020-10-18 11:25:16 +0800

    test

--- mpx-web/app.js
+++ mpx-web/app.js
@@ -24290,9 +24290,7 @@ var MpxScroll = function () {
       this.move(bounceTime, stop, 0);
       this.isRefresh = false;
       this.legacyY = 0;
-      this.pageScrollTo({
-        scrollTop: 1000
-      });
+      var position = Object(_dom__WEBPACK_IMPORTED_MODULE_6__[/* getScrollTop */ "c"])();
     }
   }, {
     key: 'move',
@@ -24325,8 +24323,8 @@ var MpxScroll = function () {
         return window.scrollTo(0, _scrollTop);
       }
 
-      // const position = getScrollTop()
-      var position = 0;
+      var position = Object(_dom__WEBPACK_IMPORTED_MODULE_6__[/* getScrollTop */ "c"])();
+      // const position = 0
 
       this.scrollAnimation.easeOutQuart(duration, position, _scrollTop, function (distance) {
         window.scrollTo(0, distance);
@@ -30547,7 +30545,7 @@ exports = module.exports = __webpack_require__(14)(false);
 
 
 // module
-exports.push([module.i, "\n.test-container {\n  margin-bottom: 20px;\n  width: 100%;\n  height: 1000px;\n  background-color: #00f;\n}\n.tabbar {\n  position: fixed;\n  top: 20px;\n  left: 0;\n  width: 50px;\n  height: 50px;\n  line-height: 50px;\n  text-align: center;\n  background: #00f;\n  color: #fff;\n}\n.box {\n  top: 0;\n  left: 0;\n  width: 100%;\n}\n", ""]);
+exports.push([module.i, "\n.test-container {\n  margin-bottom: 20px;\n  width: 100%;\n  height: 1000px;\n  background-color: #f00;\n}\n.tabbar {\n  position: fixed;\n  top: 20px;\n  left: 0;\n  width: 50px;\n  height: 50px;\n  line-height: 50px;\n  text-align: center;\n  background: #00f;\n  color: #fff;\n}\n.box {\n  top: 0;\n  left: 0;\n  width: 100%;\n}\n", ""]);
 
 // exports
 
